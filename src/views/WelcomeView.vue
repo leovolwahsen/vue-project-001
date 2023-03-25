7d5ca876e3eae2ce286c0066003c6598f7c3b1f0
Leo Volwahsen <volwahsenl@gmail.com>
2023-03-25 15:21:05 +0100

    button for more info dadded on WelcomeView.vue site

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,6 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const greeting = 'Welcome to this site'
+const message = ref('This site contains information about available jobs.')
+
+const changeMessage = () => {
+  message.value = 'This site also contains skills you need for IT jobs.'
+}
+</script>
+
 <template>
-  <div>
-    <p>This is the welcome page.</p>
+  <div class="welcome">
+    <h2>{{ greeting }}</h2>
+    <p>{{ message }}</p>
+
+    <button @click="changeMessage">more info</button>
   </div>
 </template>
+
+<style scoped>
+h2 {
+  color: olive;
+}
+
+p {
+  color: #aaa;
+  font-style: italic;
+}
+</style>
