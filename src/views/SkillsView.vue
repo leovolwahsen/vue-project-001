a5a88f30c08f50fded06768743ab557be8593d60
Leo Volwahsen <volwahsenl@gmail.com>
2023-03-25 18:20:26 +0100

    added skills as well to page under nav Skills

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,6 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios'
+const skills: ISkill[] = (await axios.get('https://edwardtanguay.vercel.app/share/skills.json'))
+  .data
+</script>
+
 <template>
-  <div>
-    <p>This is the skills page.</p>
+  <div class="skills">
+    <p>There are {{ skills.length }} skills.</p>
+    <ul>
+      <li v-for="skill in skills">
+        <a target="_blank" :href="skill.url">{{ skill.name }}</a> - {{ skill.description }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<style scoped>
+li {
+  color: #ccc;
+  font-size: 1.3rem;
+}
+a {
+  color: rgb(218, 218, 128);
+}
+</style>
