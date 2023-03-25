bec1b9058303833de85ea76d20666acbd3d90142
Leo Volwahsen <volwahsenl@gmail.com>
2023-03-25 16:53:51 +0100

    backtracking to point of faliour, at this point all nav are usable

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
+import type { IJob } from '@/interfaces'
 import axios from 'axios'
 
-const jobs: IJob[] = (await axios.get('https://edwardtanguay.vercel.app/share/jobs.json')).data
+//const jobs: IJob[] = (await axios.get('https://edwardtanguay.vercel.app/share/jobs.json')).data
 </script>
+
 <template>
   <div>
     <p>This is the jobs page.</p>
-    <p>There are {{ jobs.length }} jobs.</p>
 
-    <ul>
-      <li v-for="job in jobs">
-        {{ job.title }} at <br />
-        <b>{{ job.company }}</b>
-      </li>
-    </ul>
+    <!-- <p>There are {{ jobs.length }} jobs.</p> -->
   </div>
 </template>
-<a target="_blank" :href="job.url">{{ job.title }}</a>
-- {{ job.company }}
+<!-- <a target="_blank" :href="job.url">{{ job.title }}</a>
+- {{ job.company }} -->
 
 <style scoped>
 li {
